d78896f9fd8ac08dc54f447cfefb74c8b8eb15fa
sunmeg <atpope5@gmail.com>
2014-10-06 16:28:12 -0400

    user adds a course and views it

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,24 @@
 class CoursesController < ApplicationController
   def new
-    @courses = Course.new
+    @course = Course.new
+  end
+
+  def create
+    @course = Course.new(course_params)
+
+    if @course.save
+      flash[:notice] = "You've successfully created a new course!"
+      redirect_to course_path(@course)
+    else
+      render 'new'
+    end
+  end
+
+  def course_params
+    params.require(:course).permit(:title, :description, :subject)
+  end
+
+  def show
+    @course = Course.find(params[:id])
   end
 end
