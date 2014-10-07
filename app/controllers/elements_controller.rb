2ee834cde9ed57995f876e6e5460f949d8c11ffc
sunmeg <atpope5@gmail.com>
2014-10-07 08:53:40 -0400

    user can add new text element

---
+++ app/controllers/elements_controller.rb
@@ -0,0 +1,22 @@
+class ElementsController < ApplicationController
+  def new
+    @element = Element.new
+  end
+
+  def create
+    @element = Element.new(element_params)
+    @assignment = Assignment.find(params[:assignment_id])
+    @element.assignment = @assignment
+
+    if @element.save
+      flash[:notice] = "Element successfully created!"
+      redirect_to course_assignment_path(@assignment.course, @assignment)
+    else
+      render 'assignments/show'
+    end
+  end
+
+  def element_params
+    params.require(:element).permit(:type, :assignment_id, :content, :title, :citation, :url)
+  end
+end
