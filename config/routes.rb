947d1228e357de89e9b2add4f7c4a469310b2db4
sunmeg <atpope5@gmail.com>
2014-10-07 16:44:37 -0400

    fixed root syntax

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root to: "home#index"
+  root "home#index"
 
   resources :courses, only: [:new, :index, :create, :show] do
     resources :assignments, only: [:new, :create, :index, :show] do
