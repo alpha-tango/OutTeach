1c6a4b7034cda58355ea8d2a73a9963938bfe173
sunmeg <atpope5@gmail.com>
2014-10-08 11:36:41 -0400

    added dependent destroy to assignments and course

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,6 +1,6 @@
 class Assignment < ActiveRecord::Base
   belongs_to :course
-  has_many :elements
+  has_many :elements, dependent: :destroy
 
   validates :title, :course, presence: true
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,5 @@
 class Course < ActiveRecord::Base
-  has_many :assignments
+  has_many :assignments, dependent: :destroy
   belongs_to :user
 
   validates :title, presence: true
