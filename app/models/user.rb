60eebf3b9b9055ed8904021af8f3b1fb9da7a3b7
alpha-tango <atpope5@gmail.com>
2014-10-10 15:34:02 -0400

    added admin authorization to edit or delete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ActiveRecord::Base
   has_many :courses
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   validates :name, presence: true, uniqueness: true, length: { maximum: 16 }
+
+  def admin?
+    role == 'admin'
+  end
 end
