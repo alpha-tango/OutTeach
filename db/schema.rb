34b92654bea1ab01966f54f11c480f2d0f339e7d
alpha-tango <atpope5@gmail.com>
2014-10-10 11:23:06 -0400

    rename correct? column to correct

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141009001407) do
+ActiveRecord::Schema.define(version: 20141010152049) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20141009001407) do
   create_table "answers", force: true do |t|
     t.integer "question_id",                 null: false
     t.string  "text",                        null: false
-    t.boolean "correct?",    default: false, null: false
+    t.boolean "correct",     default: false, null: false
   end
 
   create_table "assignments", force: true do |t|
