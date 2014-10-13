09f6771037fbf1494b1fdab6ba4cb805fde1c0b3
alpha-tango <atpope5@gmail.com>
2014-10-13 12:46:54 -0400

    failing update question spec

---
+++ spec/features/update_question_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+feature 'update a question', focus: true do
+
+  before(:each) do
+    @question = FactoryGirl.create(:question)
+    @correct_answer = FactoryGirl.create(:answer, text: "correct answer", question: @question)
+    @wrong_answers = FactoryGirl.create_list(:answer, 3, question: @question)
+    sign_in_as (@question.user)
+  end
+
+  scenario 'update question with valid information' do
+    visit edit_question_path(@question)
+
+    fill_in "Question text", with: "new text"
+    fill_in "Correct Answer", with: "new correct answer"
+    fill_in 'wrong_answers_0_text', with: "so wrong 1"
+    fill_in 'wrong_answers_1_text', with: "so wrong 2"
+    fill_in 'wrong_answers_2_text', with: "so wrong 3"
+    click_button "UPDATE QUESTION"
+
+    expect(page).to have_content("updated")
+    expect(page).to have_content("new text")
+    expect(page).to have_content("new correct answer")
+    expect(page).to have_content("so wrong 1")
+    expect(page).to have_content("so wrong 2")
+    expect(page).to have_content("so wrong 3")
+
+    expect(page).to_not have_content(@question.text)
+    expect(page).to_not have_content(@correct_answer.text)
+    @wrong_answers.each do |answer|
+      expect(page).to_not have_content(answer.text)
+    end
+  end
+end
