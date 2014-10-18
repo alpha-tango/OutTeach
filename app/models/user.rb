6c6b153f6dcfec0a6ecac42c6f02253133169cef
alpha-tango <atpope5@gmail.com>
2014-10-18 17:19:49 -0400

    add enrollments table model and spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_many :courses
+  has_many :enrollments
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141018192758) do
+ActiveRecord::Schema.define(version: 20141018211416) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,13 @@ ActiveRecord::Schema.define(version: 20141018192758) do
   end
 
   add_index "elements", ["assignment_id"], name: "index_elements_on_assignment_id", using: :btree
+
+  create_table "enrollments", force: true do |t|
+    t.integer  "user_id",    null: false
+    t.integer  "course_id",  null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "questions", force: true do |t|
     t.integer  "quiz_id",    null: false
